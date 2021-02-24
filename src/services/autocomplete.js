ba19bbd4b4dd31781d7ee0b2b99905824c873063
Pablo Herzberg <37887593+pabloherzberg@users.noreply.github.com>
2021-02-24 11:46:25 -0300

    Update autocomplete.js

--- src/services/autocomplete.js
+++ src/services/autocomplete.js
@@ -1,6 +1,9 @@
 import { apiGoogle } from ".";
 
-const API_KEY = "AIzaSyAE3WueCHYERNVW9UC9aSefMiAYIi9TtIk";
+
+//const API_KEY = "AIzaSyAE3WueCHYERNVW9UC9aSefMiAYIi9TtIk";
+
+const API_KEY = "AIzaSyAh6Hf_-RISe-ibmvmaq2joSTt4dPPgHEs"
 
 export const getAddress = (address) => {
   return apiGoogle.get(
